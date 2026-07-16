192394ce734ce9e32b403e7f0977d946e7505db4
Md Fahimul Kabir Chowdhury <66033826+fahimulkabir@users.noreply.github.com>
2026-07-16 14:37:17 -0500

    Update sitemap generation to remove priority values
    Removed priority from routes in sitemap generation.

--- scripts/generateSitemap.js
+++ scripts/generateSitemap.js
@@ -39,20 +39,22 @@ function getSiteUrl() {
 const finalUrl = getSiteUrl();
 
 const routes = [
-  { path: "/", priority: 1.0 },
-  { path: "/research", priority: 0.9 },
-  { path: "/publications", priority: 0.9 },
-  { path: "/members", priority: 0.8 },
-  { path: "/news", priority: 0.8 },
+  { path: "/" },
+  { path: "/research" },
+  { path: "/publications" },
+  { path: "/members" },
+  { path: "/news" },
 ];
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset 
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 ${routes
   .map(
     (r) => `  <url>
     <loc>${finalUrl}${r.path}</loc>
-    <priority>${r.priority}</priority>
   </url>`
   )
   .join("\n")}
